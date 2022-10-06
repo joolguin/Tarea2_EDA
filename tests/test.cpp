7ab7e43a49d8e630e540b85ac87f83d295f541f1
Josefa <jmolguin@miuandes.cl>
2022-10-05 22:23:04 -0300

    terminado

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,11 +8,8 @@ int main(int nargs, char** vargs){
 	cout<<"ingrese el nombre del HTML"<<endl;
 	cin>>name;
 	string filename("../data/"+name+".html");
-	//io::readTextFile(filename);
 	cout<<endl;
 	string const text = io::removeSpace(filename);
 	io::verifyHTML(text, name);
-	
-
 	return 0;
 }
