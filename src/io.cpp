cf27da9798f5c2ab55f54edcbe01797b68614b4f
Josefa <101612566+joolguin@users.noreply.github.com>
2022-10-06 18:34:26 -0300

    Update io.cpp

--- src/io.cpp
+++ src/io.cpp
@@ -33,78 +33,70 @@ namespace io {
 		return no_space;
 	}
 
+
 	void verifyHTML(const string &text, string name){
 		ofstream myText;
 		myText.open(name+".log", ios::app);
 		stack <string> pila;
-		int Stop = 1;
-		int p = 0;
 		int error = 0;
-		int count_tags=1;
-		
-		while (p < text.size() && Stop){
-			for (size_t i = 0; i < text.size(); i++){
-			const auto* character = &text.at(i);
-				if(*character == '<'){
-					string op_tag;
-					string clo_tag;
+		int count_tags= 1;
+		int stop = 1;
 
-					if (*(character + 1) != '/'){
+		for (size_t i = 0; i < text.size(); i++){
+		const auto* character = &text.at(i);
+			if(*character == '<'){
+				string op_tag;
+				string clo_tag;
+
+				if (*(character + 1) != '/'){
+					i++;
+					character++;
+
+					while(*character != '>'){
+						op_tag += *character;
 						i++;
 						character++;
-
+					}
+					pila.push(op_tag);
+					count_tags++;
+					}
+				else{
+					string tag = pila.top();
+					if (*(character + 1) == '/'){
+						i++;
+						character+=2;
 						while(*character != '>'){
-							op_tag += *character;
+							clo_tag += *character;
 							i++;
 							character++;
 						}
-						pila.push(op_tag);
-						count_tags++;
 					}
-					else{
-						if(pila.empty()){
-							myText <<"faltan tags para cerrar"<<endl;
-							Stop = 0;
+					if (tag != clo_tag){
+						myText<<"En la línea "<<count_tags<<endl;
+						myText <<"Se esperaba"<<" </"+tag+"> en lugar de un </"+clo_tag+"> "<<endl;
+						error++;
+						stop = 0;
+						break;
 						}
-						else{
-							string tag = pila.top();
-							if (*(character + 1) == '/'){
-							i++;
-							character+=2;
-							while(*character != '>'){
-								clo_tag += *character;
-								i++;
-								character++;
-								}
-							}
-							if (tag != clo_tag){
-								myText<<"En la línea "<<count_tags<<endl;
-								myText <<"Se esperaba"<<" </"+tag+">"<<endl;
-								error++;
-								break;
-							}
-							if (tag == clo_tag) {
-								myText<< "tag <"<<tag<<"> ok"<<endl;
-								Stop=0;
-							}
-							pila.pop();
-						}
+					if (tag == clo_tag) {
+						myText<< "tag <"<<tag<<"> ok"<<endl;
+						pila.pop();
 					}
 				}
 			}
-			p++;
 		}
-	
-		if (pila.size()!=0){
+		
+		if (!pila.empty() && stop == 1){
 			myText<<"En la linea "<<count_tags<<endl;
 			myText<<"<"+pila.top()+">"<<" no tiene un closing tag"<<endl;
 			error++;
 		}
+		
 		if (error==0){
 		myText<<error<<" errores"<<endl;
 		}
 		myText.close();
 	}
-
 }
+
 
